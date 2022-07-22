68275a9b521926c93922cfe99ffb81603d25707d
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-07-22 20:51:06 +0300

    fix 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,16 @@
 
 //вариант 1
 
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
+// const textInput = document.querySelector("#name-input");
+// const output = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+// textInput.addEventListener("input", (event) => {
+//   output.textContent = event.currentTarget.value;
     
-    if (textInput.textContent = "") {
-        output.textContent = Anonymous
-    }
-});
+//     if (textInput.textContent = "") {
+//         output.textContent = Anonymous
+//     }
+// });
 
 
 
@@ -23,6 +23,20 @@ textInput.addEventListener("input", (event) => {
 //     output.textContent = textInput.value;
     
 //     if (textInput.textContent = "") {
-//         output.textContent = Anonymous
+//         output.textContent === "Anonymous"
 //     }
 // });
+
+
+
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', event => {
+  nameOutput.textContent = event.currentTarget.value;
+
+  if (nameOutput.textContent === '') {
+    nameOutput.textContent = 'Anonymous';
+  }
+});
